282df1e5048a67de1fcc8e2ede6f75c64f0668b5
madrus <madrus@gmail.com>
2016-04-03 16:18:31 +0200

    list_authors methed added, overwrite save method on Book object added

--- readit/books/models.py
+++ readit/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -10,7 +11,18 @@ class Book(models.Model):
     is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.authors.all()])
+
+    # override the default save() method
+    def save(self, *args, **kwargs):
+        if (self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
+
+        # base (super) class save method
+        super(Book, self).save(*args, **kwargs)
 
 class Author(models.Model):
     name = models.CharField(max_length=70, help_text="Use pen name, not real name",
