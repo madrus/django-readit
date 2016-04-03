357b74c591cc10afbd27320ce9fb9bd9dc1d42ac
madrus <madrus@gmail.com>
2016-04-03 19:42:17 +0200

    created our first url and view

--- readit/readit/urls.py
+++ readit/readit/urls.py
@@ -8,14 +8,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', readit.views.home, name='home'),
-    # url(r'^readit/', include('readit.readit.urls')),
+from books.views import list_books
 
+urlpatterns = [
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', list_books, name='books'),
 ]
