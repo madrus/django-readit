104318fe843ad3ed3eaed16c529c8e134263ba59
madrus <madrus@gmail.com>
2016-04-04 14:37:30 +0200

    added login and logout views

--- readit/readit/urls.py
+++ readit/readit/urls.py
@@ -1,28 +1,33 @@
 """
-Definition of urls for readit.
+Definition of urls for readit
 """
 
 from django.conf.urls import include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from books.views import (AuthorDetail, AuthorList, BookDetail, CreateAuthor,
                          list_books, review_book, ReviewList,
                         )
 
-urlpatterns = [
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+admin.autodiscover()
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    # Auth
+    url(r'^logout/$', auth_views.logout, {'next_page': 'books'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+
+    # Admin
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')), # enable admin documentation
+    url(r'^admin/', include(admin.site.urls)), # enable the admin panel
+
+    # Custom
     url(r'^$', list_books, name='books'),
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
-    url(r'^authors/add/$', CreateAuthor.as_view(), name='add-author'),
+    url(r'^authors/add/$', login_required(CreateAuthor.as_view()), name='add-author'),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
-    url(r'^review/$', ReviewList.as_view(), name='review-books'),
+    url(r'^review/$', login_required(ReviewList.as_view()), name='review-books'),
+    # see @login_required for this function view in views.py
     url(r'^review/(?P<pk>[-\w]+)/$', review_book, name='review-book'),
 ]
