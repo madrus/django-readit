8d892d34c0dd459966d8968632707ef0e253ae15
madrus <madrus@gmail.com>
2016-04-04 16:19:45 +0200

    added demo test, created fake factories

---
+++ readit/books/tests/tests.py
@@ -0,0 +1,17 @@
+import django
+from django.test import TestCase
+
+class DemoTests(TestCase):
+    """Tests for the application views."""
+
+    # Django requires an explicit setup() when running tests in PTVS
+    @classmethod
+    def setUpClass(cls):
+        django.setup()
+        super(DemoTests, cls).setUpClass()
+
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
