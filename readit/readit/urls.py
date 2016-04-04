fb9a8d326f40b0f782f19c3610c7d0361264250e
madrus <madrus@gmail.com>
2016-04-04 12:10:26 +0200

    added create author page, refactor form templates

--- readit/readit/urls.py
+++ readit/readit/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from books.views import (AuthorDetail, AuthorList, BookDetail,
+from books.views import (AuthorDetail, AuthorList, BookDetail, CreateAuthor,
                          list_books, review_book, ReviewList,
                         )
 
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^$', list_books, name='books'),
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
+    url(r'^authors/add/$', CreateAuthor.as_view(), name='add-author'),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
     url(r'^review/$', ReviewList.as_view(), name='review-books'),
     url(r'^review/(?P<pk>[-\w]+)/$', review_book, name='review-book'),
